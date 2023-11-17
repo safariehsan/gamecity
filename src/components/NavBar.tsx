e10040936929273aa1a2d9a0804baab5cbb8050d
Ehsan Safari <ehsansafari@hotmail.com>
2023-11-17 20:42:52 +0100

    Color Mode Switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,23 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import LogoImg from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="5">
       <Image src={LogoImg} alt="logo" boxSize="60px" />
-      <Text fontSize="xl" fontWeight="bold">
-        GameCity
-      </Text>
+      <HStack>
+        <Text fontSize="xl" fontWeight="bold" color="orange">
+          ~
+        </Text>
+        <Text fontSize="xl" fontWeight="bold">
+          GameCity
+        </Text>
+        <Text fontSize="xl" fontWeight="bold" color="orange">
+          ~
+        </Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 };
