023be0166f6c78c42275c4f806261b2a30cdb175
Ehsan Safari <ehsansafari@hotmail.com>
2023-11-17 19:34:53 +0100

    Design Basic Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // larger than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Sidebar
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="purple">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
